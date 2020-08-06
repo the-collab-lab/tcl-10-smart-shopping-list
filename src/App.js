c81b5f6d1a4477ce0f697ed4cc44cce08d398968
Gabriela Moreno <gabrielaxmoreno@gmail.com>
2020-08-06 15:34:58 -0700

    change in firestone sb shows up on the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import * as firebase from 'firebase';
 import 'firebase/firestore';
 import './App.css';
+
+import { TextField, Button } from '@material-ui/core';
 
 let app = firebase.initializeApp({
   apiKey: 'AIzaSyDFXBbPe8ZUkuAIc4Ep-BGbesyTW4yPy-g',
@@ -16,30 +17,67 @@ let app = firebase.initializeApp({
 
 let db = firebase.firestore();
 
-db.collection('shopping-list')
-  .get()
-  .then(results => {
-    results.forEach(result => {
-      console.log(`${result.id} => ${JSON.stringify(result.data())}`);
+function loadResults() {
+  return db
+    .collection('shopping-list')
+    .get()
+    .then(results => {
+      return results.docs.map(result => ({
+        id: result.id,
+        data: result.data(),
+      }));
     });
-  });
+}
+
+function addItemToShoppingList(name) {
+  console.log(name);
+  return db.collection('shopping-list').add({ name });
+}
 
 function App() {
+  let [results, setResults] = useState([]);
+  let [name, setName] = useState('');
+  function refreshResults() {
+    loadResults().then(data => setResults(data));
+  }
+
+  function handleSubmitForm(event) {
+    event.preventDefault();
+    if (name.length > 0) {
+      addItemToShoppingList(name).then(refreshResults);
+    }
+  }
+
+  if (results.length === 0) {
+    refreshResults();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ background: '#fff', padding: '40px', borderRadius: 5 }}>
+          <ul style={{ color: 'black' }}>
+            {results.map(result => (
+              <li key={result.id}>{result.data.name}</li>
+            ))}
+          </ul>
+
+          <form onSubmit={handleSubmitForm}>
+            <TextField
+              id="standard-basic"
+              label="Item"
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              disabled={name.length === 0}
+            >
+              Submit
+            </Button>
+          </form>
+        </div>
       </header>
     </div>
   );
