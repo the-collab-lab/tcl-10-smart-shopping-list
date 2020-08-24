b6f10f457ac4041bdad2f4e452a6fd330007baba
Tiana <Tiana.marie013@gmail.com>
2020-08-24 14:21:33 -0600

    add search field

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const List = ({ token, results }) => {
+  const [searchTerm, setSearchTerm] = useState('');
   return (
     <>
+      <form>
+        <label htmlFor="searchField">Search</label>
+        <br />
+        <input
+          onChange={event => setSearchTerm(event.target.value)}
+          value={searchTerm}
+          id="searchField"
+        ></input>
+      </form>
       {results.length === 0 ? (
         <>
           <header>Smart Shopping List</header>
