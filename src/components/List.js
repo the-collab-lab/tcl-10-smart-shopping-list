bc1b3805c193264ab9f4e44e2eed5f16d2455b1c
“Quinn <quinnlashinsky@gmail.com>
2020-08-26 11:04:34 -0400

    Remove unnecessary form element

--- src/components/List.js
+++ src/components/List.js
@@ -4,18 +4,16 @@ import { NavLink } from 'react-router-dom';
 const List = ({ results, setSearchTerm, searchTerm }) => {
   return (
     <>
-      <form>
-        <label htmlFor="searchField">Search</label>
-        <br />
-        <input
-          onChange={event => setSearchTerm(event.target.value)}
-          value={searchTerm}
-          id="searchField"
-        ></input>
-        <button disabled={searchTerm === ''} onClick={() => setSearchTerm('')}>
-          x
-        </button>
-      </form>
+      <label htmlFor="searchField">Search</label>
+      <br />
+      <input
+        onChange={event => setSearchTerm(event.target.value)}
+        value={searchTerm}
+        id="searchField"
+      ></input>
+      <button disabled={searchTerm === ''} onClick={() => setSearchTerm('')}>
+        x
+      </button>
       {results.length === 0 ? (
         <>
           <header>Smart Shopping List</header>
