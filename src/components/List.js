8c85e67e5663391a5ffc01464820ea2efe2721a7
Tiana <Tiana.marie013@gmail.com>
2020-08-25 18:27:38 -0600

    margin added to input

--- src/components/List.js
+++ src/components/List.js
@@ -4,18 +4,6 @@ import { NavLink } from 'react-router-dom';
 const List = ({ results, setSearchTerm, searchTerm }) => {
   return (
     <>
-      <form>
-        <label htmlFor="searchField">Search</label>
-        <br />
-        <input
-          onChange={event => setSearchTerm(event.target.value)}
-          value={searchTerm}
-          id="searchField"
-        ></input>
-        <button disabled={searchTerm === ''} onClick={() => setSearchTerm('')}>
-          x
-        </button>
-      </form>
       {results.length === 0 ? (
         <>
           <header>Smart Shopping List</header>
@@ -25,15 +13,32 @@ const List = ({ results, setSearchTerm, searchTerm }) => {
           </NavLink>
         </>
       ) : null}
-      <ul style={{ color: 'black' }}>
-        {results
-          .filter(result =>
-            result.name.toLowerCase().includes(searchTerm.toLowerCase()),
-          )
-          .map(result => (
-            <li key={result.id}>{result.name}</li>
-          ))}
-      </ul>
+      <div>
+        <form>
+          <label htmlFor="searchField">Search</label>
+          <input
+            onChange={event => setSearchTerm(event.target.value)}
+            value={searchTerm}
+            id="searchField"
+            style={{ margin: 10 }}
+          ></input>
+          <button
+            disabled={searchTerm === ''}
+            onClick={() => setSearchTerm('')}
+          >
+            x
+          </button>
+        </form>
+        <ul style={{ color: 'black' }}>
+          {results
+            .filter(result =>
+              result.name.toLowerCase().includes(searchTerm.toLowerCase()),
+            )
+            .map(result => (
+              <li key={result.id}>{result.name}</li>
+            ))}
+        </ul>
+      </div>
     </>
   );
 };
