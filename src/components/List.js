c6f1cedb25670ae78ef4298e9c450be2fc757b4a
“Quinn <quinnlashinsky@gmail.com>
2020-08-31 13:29:54 -0400

    Add examplePurchseDates array to mock purchaseDates data. Add calculatFrequency function which currently finds difference in time between purchaseDates, finds the mean of that value and finds the standard deviation of that value as a population.

--- src/components/List.js
+++ src/components/List.js
@@ -3,18 +3,44 @@ import { NavLink } from 'react-router-dom';
 import styles from '../List.module.css';
 import { updatePurchaseDate, updateFirestore } from '../lib/firebase.js';
 
+let examplePurchaseDates = [
+  1598892792083,
+  1598287992083,
+  1597078392083,
+  1594486392083,
+  1594313592083,
+];
+
 const List = ({ results, setSearchTerm, searchTerm, token }) => {
   function handleOnCheck(event) {
-    updatePurchaseDate(token, event.target.value).then(
-      updateFirestore(token, event.target.value),
-    );
+    // updatePurchaseDate(token, event.target.value)
+    //   .then(() => {
+    console.log(calculateFrequency(examplePurchaseDates));
+    // updateFirestore(token, event.target.value),
+    // });
   }
 
   function checkTime(time) {
     return Date.now() - time <= 86400000; //number of milliseconds equal to 24 hours
   }
 
-  function calculateFrequency(results) {}
+  function calculateFrequency(results) {
+    const arr = [];
+
+    for (let i = 0; i < results.length - 1; i++) {
+      arr.push(Math.abs(Math.floor(results[i + 1] - results[i])));
+    }
+
+    const mean = arr.reduce((acc, val) => acc + val, 0) / arr.length;
+
+    const standardDeviation = Math.sqrt(
+      arr
+        .reduce((acc, val) => acc.concat((val - mean) ** 2), [])
+        .reduce((acc, val) => acc + val, 0) /
+        (arr.length - 1),
+    );
+  }
+
   return (
     <div className={styles['list-container']}>
       <header>Smart Shopping List</header>
