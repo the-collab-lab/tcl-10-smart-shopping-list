0f103abccbf93131483651f386fa13caf18d2b30
Tiana <Tiana.marie013@gmail.com>
2020-09-08 21:31:08 -0500

    Build details page
     Added build out information; details.name, working on purchase dates

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
 const Details = ({ details, setDetails }) => {
+  const lastPurch = Math.max(...details.purchaseDates);
+  let [lastMonth, lastDate, lastYear] = new Date(lastPurch)
+    .toLocaleDateString()
+    .split('/');
+  console.log(new Date(lastPurch));
+
   return (
     <div className="background">
       <div className="modal">
         <button onClick={() => setDetails({})}>
           This will be a back arrow
         </button>
-        <h4>I am a modal</h4>
+        <h2>Smart Shopping List</h2>
+        <h3>{details.name}</h3>
+        <h4>Last Purchased: </h4>
+        <h4>Next Purchuse date: </h4>
       </div>
     </div>
   );
