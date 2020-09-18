ff21814e7506931743d9891a4c03128f40530fbd
Tiana <Tiana.marie013@gmail.com>
2020-09-18 09:14:59 -0600

    Nav Bar Component
     Nav Bar enagbled and fit to size depending on screen size.
    
     Links added to Nav Bar
    
     would love advice on how to create more of a devider between the tabs and changing the color once we decide on one.

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,50 +1,3 @@
-// import React from 'react';
-// import { NavLink } from 'react-router-dom';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Paper from '@material-ui/core/Paper';
-// import Tabs from '@material-ui/core/Tabs';
-// import Tab from '@material-ui/core/Tab';
-// import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
-// import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-
-// const useStyles = makeStyles({
-//   root: {
-//     flexGrow: 1,
-//     maxWidth: 500,
-//   },
-// });
-
-// function BottomNav() {
-
-//   const classes = useStyles();
-//   const [value, setValue] = React.useState(0);
-
-//   const handleChange = (event, newValue) => {
-//     setValue(newValue);
-//   };
-
-//   return (
-//     <Paper square className={classes.root}>
-//       <Tabs
-//        value={value}
-//                onChange={handleChange}
-//                variant="fullWidth"
-//                indicatorColor="primary"
-//                textColor="primary"
-//                aria-label="icon tabs example"
-//                >
-//           <Tab icon={<FormatListBulletedIcon/>} aria-label="list"  exact to="/list" activeClassName="selected-view"/>
-
-//           <Tab icon={<AddCircleOutlineIcon/>} aria-label="Add" exact to="/add-item" activeClassName="selected-view"/>
-
-//       </Tabs>
-
-//     </Paper>
-//   );
-// }
-
-// export default BottomNav;
-
 import React from 'react';
 
 import { Link } from 'react-router-dom';
@@ -57,9 +10,11 @@ import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    backgroundColor: theme.palette.background.paper,
-    width: 375,
+    minWidth: 375,
+    maxWidth: 3000,
+    height: 30,
   },
+  tab: {},
 }));
 
 export default function FullWidthTabs() {
@@ -70,12 +25,8 @@ export default function FullWidthTabs() {
     setValue(newValue);
   };
 
-  // const handleChangeIndex = (index) => {
-  //   setValue(index);
-  // };
-
   return (
-    <div className={classes.root}>
+    <div lassName={classes.root}>
       <AppBar position="static" color="default">
         <Tabs
           value={value}
